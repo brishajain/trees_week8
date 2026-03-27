77c436ddf8e5907d41f60d76c616df6cc997f185
Brisha Jain <brishajain02@gmail.com>
2026-03-27 11:39:12 -0500

    changes as of friday-2

--- src/test/java/edu/ttap/TreeTest.java
+++ src/test/java/edu/ttap/TreeTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import edu.ttap.trees.Tree;
+
 public class TreeTest {
     @Test
     public void emptyTreeSizeTest() {
