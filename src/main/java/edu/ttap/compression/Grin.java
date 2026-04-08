c65beb319941b6233f977a69eaf8a27216323aae
littlecarri <carrieannelittle61@gmail.com>
2026-04-07 21:13:07 -0500

    "Fixed issues in Grin.java"
    Co-authored-by: jd-carpio <jd-carpio@users.noreply.github.com>
    Co-authored-by: brishajain <brishajain@users.noreply.github.com>

--- src/main/java/edu/ttap/compression/Grin.java
+++ src/main/java/edu/ttap/compression/Grin.java
@@ -1,5 +1,7 @@
 package edu.ttap.compression;
 
+import java.io.IOException;
+
 /**
  * The driver for the Grin compression program.
  */
@@ -10,19 +12,43 @@ public class Grin {
      * @param infile the file to decode
      * @param outfile the file to ouptut to
      */
-    public static void decode(String infile, String outfile) {
-        // check for magic number
-        
+
+    public static final int MAGIC_NUMBER = 1846;
+
+    public static void decode(String infile, String outfile) throws IllegalArgumentException {
+        try {
+            BitInputStream in = new BitInputStream(infile);
+            BitOutputStream out = new BitOutputStream(outfile);
+            
+            if(in.readBits(32) != MAGIC_NUMBER) {
+                throw new IllegalArgumentException("Invalid .grin file");
+            }
+
+            HuffmanTree tree = new HuffmanTree(in);
+            tree.decode(in, out);
+
+            in.close();
+            out.close();
+
+        } catch (IOException e){
+            return;
+        } 
     }
 
+    
     /**
      * The entry point to the program.
      * @param args the command-line arguments.
      */
     public static void main(String[] args) {
-        if(args.length != 2)
-            return;
+        if(args.length != 2) {
+            throw new IllegalArgumentException("Incorrect number of command line arguments");
+        }  
+        try {
         decode(args[0], args[1]); 
         System.out.println("Usage: java Grin <infile> <outfile>");
+        } catch (IllegalArgumentException b) {
+            return;
+        } 
     }
 }
